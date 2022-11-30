000e12174b58a04aa50e874c6eb3d576651e326f
Jmansito <j_mansito@icloud.com>
2022-11-30 11:06:08 -0700

    login styling

--- login.php
+++ login.php
@@ -6,32 +6,29 @@
         <title>Add Resume Form</title>
         <link rel="stylesheet" href="style.css" />
     </head>
-    <body>
-    <main>
-        <h1>Login</h1>
-        <form action="." method="post" id="add_product_form">
-            <input type="hidden" name="action" value="login">
+    <body class="login_body">
+    <div class="login_form">
+        <div class="login-wrapper">
+            <form class="form_login" action="." method="post" id="add_product_form">
+                <h1 class="title">Sign in</h1>
+                <input class="login_input" type="hidden" name="action" value="login">
 
-            <p>
-                <label>User Name:
-                    <input type="text" name="user_name" />
-                </label>
-            </p>
-            <p>
-                <label>Password:
-                    <input type="password" name="password" />
-                </label>
-            </p>
+                <div class="login_div">
+                        <input type="text" class="login_input" name="user_name" />
+                        <label for="" class="login_label">Username</label>
+                </div>
+                <div class="login_div">
+                        <input type="password" class="login_input" name="password" />
+                        <label for="" class="login_label">Password</label>
+                </div>
+                <input type="submit" class="login_button" value="Login" />
+                <p class="login_link">Don't have an account? <a href="signup.php">Sign up</a></p>
+            </form>
 
-            <label>
-                <input type="submit" value="Login" />
-            </label>
-            <br>
-        </form>
-        <p>Don't have an account? <a href="signup.php">Sign up</a></p>
+        </div>
 
+    </div>
 
-    </main>
     </body>
 
 
