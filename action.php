b8d20753558028da3c79c62edf0224b8f9470daa
Impulse12600g <k.lal.1078780@gmail.com>
2022-11-30 22:18:28 -0700

    resume image save work

--- action.php
+++ action.php
@@ -15,17 +15,17 @@ function get_categories(){
     $statement->closeCursor();
     return $applicants;
 }
-function add_resume($user_name,$f_name, $l_name,$dob, $email, $phone_number, $previous_job, $previous_job_desc,$previous_job_sd,
+function add_resume($user_name, $user_id, $f_name, $l_name,$dob, $email, $phone_number, $previous_job, $previous_job_desc,$previous_job_sd,
                     $previous_job_ed, $skill1, $skill2, $skill3, $profile_pic, $resume_pdf){
     global $db;
 
     $query = 'INSERT INTO `Resume`
-          (Resume.user_name, Resume.f_name, Resume.l_name, Resume.dob, Resume.email,
+          (Resume.user_name, Resume.user_id, Resume.f_name, Resume.l_name, Resume.dob, Resume.email,
                Resume.phone_number, Resume.previous_job, Resume.previous_job_desc,
                Resume.previous_job_sd, Resume.previous_job_ed, Resume.skill1,
                Resume.skill2, Resume.skill3, Resume.profile_pic, Resume.resume_pdf)
             VALUES
-                (:user_name, :f_name, :l_name, :dob, :email, :phone_number, :previous_job,
+                (:user_name, :user_id, :f_name, :l_name, :dob, :email, :phone_number, :previous_job,
                     :previous_job_desc, :previous_job_sd, :previous_job_ed,
                     :skill1, :skill2, :skill3, :profile_pic, :resume_pdf)';
     $statement = $db->prepare($query);
@@ -44,6 +44,7 @@ function add_resume($user_name,$f_name, $l_name,$dob, $email, $phone_number, $pr
     $statement->bindValue('skill3', $skill3);
     $statement->bindValue('profile_pic', $profile_pic);
     $statement->bindValue('resume_pdf', $resume_pdf);
+    $statement->bindValue('user_id', $user_id);
     $statement->execute();
     $statement->closeCursor();
 
